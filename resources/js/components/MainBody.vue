79fcf08e7eedd628ed66abe1d8e47170ddcce3b4
Jay <migoermita@JayTheGreat.local>
2024-09-24 11:39:13 +0800

    Initial commit

---
+++ resources/js/components/MainBody.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>Initial commit
+        <pre>{{currentUser}}</pre>
+        <v-btn @click="logout">logout</v-btn>
+    </div>
+</template>
+<script>
+	export default {
+        methods: {
+            logout() {
+                this.$store.dispatch('auth/logout').then((res, rej) => {
+                    this.$router.push({
+                        name: 'login'
+                    })
+                })
+            }
+        },
+        computed: {
+            currentUser() {
+                return this.$store.getters.getCurrentUser;
+            }
+        }
+	}
+</script>
